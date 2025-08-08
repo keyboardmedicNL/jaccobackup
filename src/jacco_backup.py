432aeec8b746f7c6b5d8d81b2a98a6fc1b2faf63
keyboardmedic <keyboardmedic@proton.me>
2025-08-08 20:42:13 +0200

    add: added samba accessible check

--- src/jacco_backup.py
+++ src/jacco_backup.py
@@ -4,7 +4,6 @@ housey_logging.configure()
 import os
 import time
 import logging
-import requests
 import config_loader
 from smb.SMBConnection import SMBConnection
 
@@ -41,7 +40,7 @@ def check_connection(hostname_to_ping: str, ping_interval: int):
             errors_remaining = 3-error_count
             
             if not error_count == 3:
-                logging.error("%s was not reachable... trying again in %s seconds for %s more times ",hostname_to_ping, ping_interval, errors_remaining)
+                logging.error("%s was not reachable... trying again in %s seconds for %s more times and waiting for %s more seconds ",hostname_to_ping, ping_interval, errors_remaining, ping_interval)
                 time.sleep(ping_interval)
 
     if error_count == 3:
@@ -60,12 +59,30 @@ def wakeonlan(server_mac: str):
 
     os.system(wol_command)
 
-def samba_up(server_adress: str, share_name: str, user_name: str, pass_word: str):
-    conn = SMBConnection(user_name, pass_word, share_name, server_adress)
-    conn.connect(server_adress)
+def samba_up(server_adress: str, share_name: str, user_name: str, pass_word: str, samba_wait_time: int):
+    logging.info("attempting connection to samba share at adress %s", server_adress)
+    error_count = 0
+    while error_count < 3:
 
-    accessible = share_name in conn.listShares()
-    print(accessible)
+        try:
+            conn = SMBConnection(user_name, pass_word, "python", server_adress)
+            conn.connect(server_adress)
+
+            for share in conn.listShares():
+                if share_name in share.name:
+                    logging.info("%s is accessible on server %s",share_name, server_adress)
+            break
+
+        except:
+            if not error_count == 3:
+                error_count = error_count+1
+                errors_remaining = 3-error_count
+                logging.error("unable to connect to samba share from server %s trying %s more times and waiting for %s seconds", server_adress, errors_remaining, samba_wait_time)
+                time.sleep(samba_wait_time)
+    if error_count == 3:
+        send_telegram_notification()
+        raise RuntimeError ("tried to connect to samba share 3 times and failed")
+
 
 def send_telegram_notification():
     None
@@ -80,15 +97,15 @@ def give_root_acces_xterm():
     None
 
 def main():
+
     loaded_config = config_loader.load_config()
 
     check_connection(loaded_config.host_name,loaded_config.ping_interval)
     enable_vpn()
     #wakeonlan(loaded_config.server_mac)
-    samba_up(loaded_config.server_adress, loaded_config.samba_share_name, loaded_config.samba_user_name, loaded_config.samba_password)
-    #time.sleep(loaded_config.wait_time_before_attempt_connect_server)
+    samba_up(loaded_config.server_adress, loaded_config.samba_share_name, loaded_config.samba_user_name, loaded_config.samba_password, loaded_config.samba_wait_time)
 
-    None
+    logging.info("script finished...")
 
 
 if __name__ == "__main__":
